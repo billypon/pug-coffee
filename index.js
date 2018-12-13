bccf7175ed3f0c0a6c0e726b8e99d76c383f4c37
billypon <billypon@gmail.com>
2018-12-13 14:32:51 +0800

    use uglify-es instead of uglify-js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const coffee = require('coffeescript');
+const uglify = require('uglify-es');
 
 module.exports = function (callback) {
   return function (str, options) {
@@ -11,7 +12,6 @@ module.exports = function (callback) {
     let js = coffee.compile(str, coffeeOptions);
 
     if (options.uglify === undefined || options.uglify) {
-      const uglify = require('uglify-js');
       const result = uglify.minify(js, { fromString: true });
       if (result.error) {
         throw result.error;
